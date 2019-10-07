f3ad72b03e640e08af53f39497e1da42d5278f36
Felipe Espinoza <felipe.espinoza.r@uchile.cl>
2019-10-07 14:12:59 +0000

    Ingrease file limit of sga

--- edx_sga/sga.py
+++ edx_sga/sga.py
@@ -80,7 +80,7 @@ class StaffGradedAssignmentXBlock(StudioEditableXBlockMixin, ShowAnswerXBlockMix
     """
     has_score = True
     icon_class = 'problem'
-    STUDENT_FILEUPLOAD_MAX_SIZE = 4 * 1000 * 1000  # 4 MB
+    STUDENT_FILEUPLOAD_MAX_SIZE = 40 * 1000 * 1000  # 40 MB
     editable_fields = ('display_name', 'points', 'weight', 'showanswer', 'solution')
 
     display_name = String(
